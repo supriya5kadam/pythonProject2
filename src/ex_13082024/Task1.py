1ccf59e3838ba9a74ba503e552a36d395c2bd0cc
Supriya <supriyasawant991@gmail,com>
2024-08-17 13:11:02 +0530

    all the files for the 13th aug are Added-Homework task

---
+++ src/ex_13082024/Task1.py
@@ -0,0 +1,11 @@
+#program of add,sum,sub,mul and div
+
+num1=int(input("Enter num1"))
+num2=int(input("Enter num2"))
+print("Maximum no is ", max(num1,num2))
+print("power is", pow (num1,num2))
+print("Sum is",num1+num2)
+print("subtraction is",num1-num2)
+print("Multiplication is",num1*num2)
+print("Division is ", f"{num1/num2: .3f}")
+
